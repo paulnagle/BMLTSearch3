2669e0339818cd397e4254f23410437337c240ba
Paul Nagle <paulnagle@gmail.com>
2020-08-11 08:04:08 +0100

    Map search page

--- re-plugin.sh
+++ re-plugin.sh
@@ -8,12 +8,12 @@ then
 
     ionic cordova platform rm ios
     # ionic cordova platform rm android
-    # ionic cordova platform rm browser
+    ionic cordova platform rm browser
 
     ionic cordova plugin rm cordova-plugin-splashscreen
     ionic cordova plugin rm cordova-plugin-statusbar
     ionic cordova plugin rm cordova-plugin-googlemaps
-    ionic cordova plugin rm cordova-plugin-advanced-http
+    #ionic cordova plugin rm cordova-plugin-advanced-http
     ionic cordova plugin rm com-badrit-base64
     ionic cordova plugin rm cordova-plugin-ionic-webview
     ionic cordova plugin rm cordova-plugin-inappbrowser
@@ -29,7 +29,7 @@ then
     ionic cordova plugin add cordova-plugin-statusbar
 #    ionic cordova plugin add cordova-plugin-googlemaps
     ionic cordova plugin add https://github.com/mapsplugin/cordova-plugin-googlemaps.git#multiple_maps
-    ionic cordova plugin add cordova-plugin-advanced-http
+    #ionic cordova plugin add cordova-plugin-advanced-http
     ionic cordova plugin add com-badrit-base64
     ionic cordova plugin add cordova-plugin-ionic-webview@latest
     ionic cordova plugin add cordova-plugin-inappbrowser
