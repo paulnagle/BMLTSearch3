4a880f3d1337fe705dfbace83c6efe31c581653f
Paul Nagle <paulnagle@gmail.com>
2020-08-12 07:57:39 +0100

    Android resources and tidyDelimiter pipe fix

--- re-plugin.sh
+++ re-plugin.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-
 if [ -z "$1" ]
 then
 
     rm -rf www
 
     ionic cordova platform rm ios
-    # ionic cordova platform rm android
+    ionic cordova platform rm android
     ionic cordova platform rm browser
 
     ionic cordova plugin rm cordova-plugin-splashscreen
     ionic cordova plugin rm cordova-plugin-statusbar
     ionic cordova plugin rm cordova-plugin-googlemaps
-    #ionic cordova plugin rm cordova-plugin-advanced-http
     ionic cordova plugin rm com-badrit-base64
     ionic cordova plugin rm cordova-plugin-ionic-webview
     ionic cordova plugin rm cordova-plugin-inappbrowser
@@ -27,9 +25,7 @@ then
 
     ionic cordova plugin add cordova-plugin-splashscreen
     ionic cordova plugin add cordova-plugin-statusbar
-#    ionic cordova plugin add cordova-plugin-googlemaps
     ionic cordova plugin add https://github.com/mapsplugin/cordova-plugin-googlemaps.git#multiple_maps
-    #ionic cordova plugin add cordova-plugin-advanced-http
     ionic cordova plugin add com-badrit-base64
     ionic cordova plugin add cordova-plugin-ionic-webview@latest
     ionic cordova plugin add cordova-plugin-inappbrowser
@@ -41,6 +37,6 @@ ionic cordova prepare ios --prod
 # ionic cordova prepare browser --prod
 
 # ionic cordova resources ios
-#ionic cordova resources android
+# ionic cordova resources android
 
 # ionic build --prod  --minifyjs   --minifycss  --optimizejs
