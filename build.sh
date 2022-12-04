500e04387af1fbc3e5793e4d2b7d2109bccdfb5b
Paul Nagle <paulnagle@gmail.com>
2022-12-04 18:37:19 +0000

    Updates to build script and fix for map on android

--- build.sh
+++ build.sh
@@ -1,38 +1,76 @@
 #!/bin/bash
 
+RED='\033[0;31m'  
+NC='\033[0m' # No Color
+
+red_text() {
+    echo -e "${RED}>>>>>>>>> [${FUNCNAME[1]}] $1${NC}"
+}
+
 usage(){
 	echo "Usage: -b (Build for browser)"
     echo "       -i (Build for ios)"
     echo "       -a (Build for android)"
     echo "       -c (Clean old build files)"
+    echo "       -r (Release build for android when passed with -a)"
 	exit 1
 }
 
 add_plugins() {
-    echo "** Adding cordova plugins"
-    ionic cordova plugin add cordova-plugin-splashscreen
+    red_text "** Adding cordova plugins"
+
+    if [[ "${PLATFORM}" != "android" ]]; then
+        ionic cordova plugin add cordova-plugin-splashscreen
+    fi
     ionic cordova plugin add cordova-plugin-statusbar
-    ionic cordova plugin add cordova-plugin-googlemaps
+    # ionic cordova plugin add cordova-plugin-googlemaps
+    ionic cordova plugin add https://github.com/mapsplugin/cordova-plugin-googlemaps.git#0b8ea76ad34fb2a202a9de1b9d0e051a82ad9443
     ionic cordova plugin add com-badrit-base64
     ionic cordova plugin add cordova-plugin-ionic-webview
     ionic cordova plugin add cordova-plugin-inappbrowser
     ionic cordova plugin add cordova-plugin-geolocation
     ionic cordova plugin add cordova-plugin-advanced-http
-    ionic cordova plugin add cordova-plugin-androidx-adapter
+    # ionic cordova plugin add cordova-plugin-androidx-adapter
 }
 
-install_deps() {
-    echo "** Installing ionic cli"
-    npm install -g --save @ionic/cli native-run cordova-res cordova
+setup_node_npm() {
+    red_text "Setting up node and npm versions"
+    
+    if [ ! -d ~/.nvm ]; then
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+    fi
 
-    echo "** Installing other npm dependencies"
+    export NVM_DIR=$HOME/.nvm;
+    source $NVM_DIR/nvm.sh;
+
+    NVM_VERSION_REQUIRED="v16.18.1"
+    NVM_VERSION_CURRENT=$(nvm version)
+    if [[ "${NVM_VERSION_CURRENT}" != "${NVM_VERSION_REQUIRED}" ]]; then
+        nvm install v16.18.1
+        nvm use v16.18.1
+    else
+        red_text "node version: ${NVM_VERSION_CURRENT}"
+    fi
+}
+
+setup_ionic() {
+    if ! [ -x "$(command -v ionic)" ]; then
+        red_text "** Installing ionic cli globally"
+        npm install -g @ionic/cli @ionic/cordova-builders native-run cordova-res cordova 
+    else
+        red_text "** Updating ionic cli globally"
+        npm update -g @ionic/cli @ionic/cordova-builders native-run cordova-res cordova 
+    fi
+}
+
+install_npm_deps() {
+    red_text "** Installing other npm dependencies"
     npm install --save \
         @ionic-native/google-maps \
         @ionic-native/base64 \
         @awesome-cordova-plugins/in-app-browser \
         @awesome-cordova-plugins/geolocation  \
         @awesome-cordova-plugins/http  \
-        @awesome-cordova-plugins/splash-screen \
         @awesome-cordova-plugins/http \
         @awesome-cordova-plugins/status-bar \
         @ngx-translate/core \
@@ -41,15 +79,22 @@ install_deps() {
         thenby \
         moment \
         moment-timezone
+
+    if [[ "${PLATFORM}" != "android" ]]; then
+        npm install --save @awesome-cordova-plugins/splash-screen 
+    fi
+
+    red_text "** Running npm audit fix"
+    npm audit fix
 }
 
 clean_old_build() {
-    rm -rf www
-
+    red_text "!! Removing cordova platforms"
     ionic cordova platform rm ios
     ionic cordova platform rm android
     ionic cordova platform rm browser
 
+    red_text "!! Removing cordova plugins"
     ionic cordova plugin rm cordova-plugin-splashscreen
     ionic cordova plugin rm cordova-plugin-statusbar
     ionic cordova plugin rm cordova-plugin-googlemaps
@@ -58,32 +103,62 @@ clean_old_build() {
     ionic cordova plugin rm cordova-plugin-inappbrowser
     ionic cordova plugin rm cordova-plugin-geolocation
     ionic cordova plugin rm cordova-plugin-advanced-http
-    ionic cordova plugin rm cordova-plugin-androidx-adapter
-    rm -rf platform/*
+    # ionic cordova plugin rm cordova-plugin-androidx-adapter
+    red_text "!! Deleting platform folder"
+    rm -rf platform
+    red_text "!! Deleting node_module folder"
+    rm -rf node_modules
+    red_text "!! Deleting plugins folder"
+    rm -rf plugins
+    red_text "!! Deleting www folder"
+    rm -rf www
 }
 
 build_for() {
     PLATFORM=$1
 
-    echo "Building for ${PLATFORM}"
-    install_deps
-    ionic cordova platform add "${PLATFORM}" --confirm --no-interactive
+    red_text ">>>> Building for ${PLATFORM}"
+    install_npm_deps
+    red_text ">>>> ionic cordova platform add ${PLATFORM} --confirm --no-interactive"
+    if [[ "${PLATFORM}" == "android" ]]; then
+        ionic cordova platform add android --confirm --no-interactive 
+    else
+        ionic cordova platform add "${PLATFORM}" --confirm --no-interactive
+    fi
+    red_text ">>>> add_plugins"
     add_plugins
-    if [[ "${PLATFORM}" != "browser" ]]; then 
-        ionic cordova resources "${PLATFORM}"
-    fi 
+    # if [[ "${PLATFORM}" != "browser" ]]; then 
+    #     echo ">>>> ionic cordova resources ${PLATFORM}"
+    #     ionic cordova resources "${PLATFORM}"
+    # fi 
+    red_text ">>>> ionic cordova prepare ${PLATFORM}"
     ionic cordova prepare "${PLATFORM}" 
-    ionic cordova build "${PLATFORM}" 
+    red_text ">>>> ionic cordova build ${PLATFORM}" 
+    if [[ "${ANDROID_RELEASE}" == "true" ]]; then
+        ionic cordova build android --release --prod
+    else
+        ionic cordova build "${PLATFORM}"
+    fi
 }
 
+########
+# main #
+########
+ANDROID_RELEASE=false
+setup_node_npm
+setup_ionic
+
 [[ $# -eq 0 ]] && usage
-while getopts "abci" option; do
+while getopts "abcir" option; do
    case $option in
       c) # Clean old build files
          clean_old_build
          ;;
       a) # Build for android
          build_for android
+         ;;
+      r) # Android release build
+         ANDROID_RELEASE=true
          ;;
       i) # Build for ios
          build_for ios
